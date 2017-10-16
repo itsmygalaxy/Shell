ea04701276ee3e25e10f1e8b45e9dcc90fa58b2c
v3nk@t <itsmygalaxy@users.noreply.github.com>
2017-10-16 12:27:40 -0500

    Update cronuser.sh

--- cronuser/cronuser.sh
+++ cronuser/cronuser.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#for user in $(clish -c "show users" | awk '{print $1}'); do
+#for user in $(clish -c "show users" | awk '{print $1}'); do ##for checkpoint
 for user in $(cut -d : -f 1 /etc/passwd); do
 echo -e "USER - $user:\n"
 more /var/spool/cron/$user
